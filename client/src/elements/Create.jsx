067749e8a1cea9e86ddfa061dd0cae44e832b2ff
alex sanchez <alexjsanchez1020@gmail.com>
2024-09-26 11:20:17 -0400

    adding pages import

--- client/src/elements/Create.jsx
+++ client/src/elements/Create.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Create(){
+    const [values, setValues] = useState({
+            name : '',
+            super: '',
+            office:'',
+            cell: '',
+            busuper: '',
+            bucell: '',
+    })
     return (
         <div className="container vh-100 vw-100 bg-primary">
             <div className="row">
                 <h3>Add Location</h3>
                 <form>
                     <div className="form-group">
-                        <label htmlFor="name">name</label>
+                        <label htmlFor="name" onChange={(e)=>setValues({...values, name: e.target.values})}></label>
                         <input type="text" name="name"/>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="number">number</label>
-                        <input type="text" name="number"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="super">super</label>
                         <input type="text" name="super"/>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="busuper">super</label>
+                        <label htmlFor="office">number</label>
+                        <input type="text" name="office"/>
                     </div>
                     <div className="form-group">
-                        <label htmlFor="bunumber">bunumber</label>
+                        <label htmlFor="cell">cell</label>
+                        <input type="text" name="cell"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="busuper">busuper</label>
+                        <input type="text" name="busuper" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="bucell">bucell</label>
+                        <input type="text" name="bucell" />
                     </div>
                 </form>
             </div>            
