02ee61d3fbfaf0ad5d8995b4c0c9ab80a47a8b8d
alex sanchez <alexjsanchez1020@gmail.com>
2024-09-26 10:58:15 -0400

    create form for adding location

--- client/src/elements/Create.jsx
+++ client/src/elements/Create.jsx
@@ -1,2 +1,33 @@
 import React from "react";
 
+function Create(){
+    return (
+        <div className="container vh-100 vw-100 bg-primary">
+            <div className="row">
+                <h3>Add Location</h3>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="name">name</label>
+                        <input type="text" name="name"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="number">number</label>
+                        <input type="text" name="number"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="super">super</label>
+                        <input type="text" name="super"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="busuper">super</label>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="bunumber">bunumber</label>
+                    </div>
+                </form>
+            </div>            
+        </div>
+    )
+}
+
+export default Create;
